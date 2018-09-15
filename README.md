9f1ca4d6dec15198de11e009396e4f02a3db7db7
chrsrcks <chrsrckes@gmail.com>
2018-09-15 04:05:52 +0200

    update readme/docu

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # TileMap
 
+TileMap(
+    images, // ( image array || tileSheet )
+    tileSize, // ( size in px )
+    array2D, // ( [[index]] )
+    pos // ( position )
+)
+
+.draw() // draw all tiles
+
+.changeTile(x, y, new value) // change image index
+
+.mouseOverTile(mouseX, mouseY, x, y) // return x, y, index
+
 <v 1.0>
 - draw a map with images
 - change tile
 
-<v 1.1> 
+<v 1.1>
+- tile sheet
+- mouse over tile (return x,y,value for change, highlighting or value conditions)
+
+<v 1.2>
 - draw objects (sprites or colored rectangles)
--- sketch.js
+++ sketch.js
@@ -13,15 +13,14 @@ function setup() {
 
   // example 1
   array2D = new Array2D(3, 3, (x,y)=> floor(random(3)));
-  console.table(array2D);
+  //console.table(array2D);
   tileMap = new TileMap(tileArray, 64, array2D, {x:64,y:64});
   tileMap.changeTile(1,1,-1);
 
   // example 2
   array2D_2 = new Array2D(3, 3, (x,y)=> floor(random(1,9)));
-  console.table(array2D_2);
-  let tileArray_2 = sheetToArray(tileSheet, 32);
-  tileMap_2 = new TileMap(tileArray_2, 64, array2D_2, {x:320,y:64});
+  //console.table(array2D_2);
+  tileMap_2 = new TileMap(tileSheet, 64, array2D_2, {x:320,y:64});
 
 }
 
